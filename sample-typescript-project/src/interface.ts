fdb37e21e0f62b70587d1d140d3039bd1196b39d
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2025-06-08 17:23:00 +0900

    引数あれこれ

--- sample-typescript-project/src/interface.ts
+++ sample-typescript-project/src/interface.ts
@@ -1,39 +1,69 @@
-// interface と type の違い
+// interfaceについて
 
+// Userインターフェースの定義
 interface User {
-    id: number;
     name: string;
-    isActive: boolean;
-    email?: string; // ?をつけることでオプショナルなプロパティになる。kotlin の nullable と同じようなイメージ
+    age: number;
+    // email: string | undefined;これと同じ意味となる
+    // emailはundefinedを許容する形となり、値がない場合は undefinedとなる
+    // null は許容しない
+    email?: string // nullableなプロパティ
 }
 
-// email プロパティは入れなくてもよい
-const user: User = {
-    id: 1,
-    name: "Johen Doe",
-    isActive: true
-};
-
-interface User2 {
-    readonly id: number; // readonly をつけることで変更できないプロパティになる
-    name: string
+// 拡張
+interface UserWithGender extends User {
+    gender?: string;
 }
 
-const user2: User2 = {
-    id: 1,
-    name: "Johen Doe"
-};
 
-// user2.id = 2; // エラー内容: Cannot assign to 'id' because it is a read-only property 読み取り専用プロパティであるため、'id' に代入することはできません。
-
-// 関数の型を定義するやり方。
-// Kotlinの場合、こちらの方が基本形
-interface Greet {
-    (name: string): string;
+function createUser(name: string, age: number, email?: string): User {
+    // ---- ユーザ作成する処理 -----
+    return {
+        name: name,
+        age: age,
+        email: email
+    }
 }
 
-const greet: Greet = (name) => {
-    return "Good morning, " + name;
+// オブジェクトを引数にする方法
+function createUser2(user: UserWithGender): UserWithGender {
+    // ---- ユーザ作成する処理 -----
+    return {
+        name: user.name,
+        age: user.age,
+        gender: user.gender,
+        email: user.email
+    }
 }
 
-console.log(greet("TypeScript"));
+// オブジェクトの分割することもできる
+// 名前付き引数にしたい場合に利用できる
+// それ用のインターフェースを定義しないといけない
+function createUser3({name, age, gender, email}: UserWithGender): User {
+    // ---- ユーザ作成する処理 -----
+    // ---- gender処理 -----
+    return {
+        name: name,
+        age: age,
+        email: email
+    }
+}
+// 基本的に位置引数で定義となる
+// JavaやKotlinのような名前付き引数はできない
+const user1: User = createUser("Alice", 20);
+console.log(user1);
+
+const user2: UserWithGender = createUser2({
+    name: "Bob",
+    age: 25,
+    gender: "main",
+})
+console.log(user2);
+
+const user3: User = createUser3({
+    name: "Charlie",
+    age: 30,
+    email: "hogehoge@example.com"
+})
+
+console.log(user3);
